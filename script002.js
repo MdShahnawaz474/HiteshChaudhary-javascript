9d4cff5e7a533f5d1e40c262d09c0b2778cfec4b
MD Shahnawaz <157542687+MdShahnawaz474@users.noreply.github.com>
2024-03-19 20:49:15 +0000

    script002

---
+++ script002.js
@@ -0,0 +1,20 @@
+//basic concept and datatypes of javascript
+//how to use const, let , var, and 
+
+//Use of const
+const accountId = 294902;
+//use of let 
+let accountEmail= "mdshahnawaz49092@gmail.com"
+//use of var
+var accountPassword= "d2902@9401";
+
+accountcity = "delhi";
+// when we use the const datatype we cant change the value its shows error in your terminal //
+// accountId=9302894;//
+
+//In var and let datatypes we can change easily anytime and terminal doesnot show error//
+
+console.log(accountId);
+
+//console.table is used for execute the programme in single array
+console.table([accountEmail, accountId, accountPassword]);
